c7198faa7306a1fab1005fc8937d4e6217a6dee4
Keshav Gill <keshavgill030@gmail.com>
2026-06-27 14:21:03 +0000

    feat: create Student entity model with encapsulation

--- src/model/Student.java
+++ src/model/Student.java
@@ -2,4 +2,48 @@ package model;
 
 public class Student {
 
+    private String studentId;
+    private String name;
+    private String email;
+    private int age;
+    private double grade;
+
+    // construtor for student
+    public Student(String studentId, String name, String email, int age, double grade) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    // getter methods
+    public String getStudentId(){
+        return studentId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString () {
+        return String.format("Student: {id = %s, name = %s, email = %s, age = %d, grade = %.1f}", studentId, name, email, age, grade);
+    }
+
+
+
+
 }
